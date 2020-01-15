48a5d9195557a4b9454825e15ab1281b7df50d6c
Attyuttam <attyuttam@gmail.com>
2020-01-15 09:21:02 +0530

    Update TerribleChandu.cpp

--- Hackerearth/Codemonk/v1/Sorting/TerribleChandu.cpp
+++ Hackerearth/Codemonk/v1/Sorting/TerribleChandu.cpp
@@ -1,1 +1,14 @@
 
+//Created by Attyuttam Saha
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>> t;
+    while(t--){
+        string str;
+        cin>>str;
+        reverse(str.begin(),str.end());
+        cout<<str<<endl;
+    }
+}
